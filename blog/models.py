b8daa18feb5df638c0b7507d159f9ca783439945
TaigaMikami <mikamimikami60@gmail.com>
2018-06-22 00:38:34 +0900

    add blog migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+  # id = 1,2,3,4
+  content = models.CharField(max_length=140, verbose_name='本文')
+  posted_date = models.DateTimeField(auto_now_add=True) # 自動記録
